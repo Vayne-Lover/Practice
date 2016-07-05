d6cdcef520ca1dae00a587f8fed3ea829c49e4db
Vayne-Lover <406378362@qq.com>
2016-07-05 16:00:22 +0800

    Use stack

--- Hanoi/Hanoi/main.cpp
+++ Hanoi/Hanoi/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <stack>
 using namespace std;
 void Move(char a,char b)
 {
@@ -34,10 +35,34 @@ void Hanoi1(int n,char a,char b,char c)
         Hanoi1(n-1, c, b, a);
     }
 }
-
+stack<int> sta[4];
+void initStack()
+{
+    for (int i=4; i>0; --i)
+    {
+        sta[1].push(i);
+    }
+}
+void StackHanoi(int n,int a,int b,int c)
+{
+    if(n>0)
+    {
+        StackHanoi(n-1, a, c, b);
+        int temp=sta[a].top();//Take care there should be a,b rather than 1,2
+        sta[a].pop();
+        sta[b].push(temp);
+        StackHanoi(n-1, c, b, a);
+    }
+}
 int main(int argc, const char * argv[]) {
     //Hanoi(3, 'A', 'B', 'C');
-    Hanoi1(3, 'a', 'b', 'c');
-    //cout<<"Hello"<<endl;
+    //Hanoi1(3, 'a', 'b', 'c');
+    initStack();
+    StackHanoi(4, 1, 2, 3);
+    while(!sta[2].empty())
+    {
+        cout<<sta[2].top()<<endl;
+        sta[2].pop();
+    }
     return 0;
 }
