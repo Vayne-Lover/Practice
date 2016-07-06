1d86055e6fc7b1a99aab18716c95300b2277b7b8
Vayne-Lover <406378362@qq.com>
2016-07-06 15:41:28 +0800

    Use list

--- ReverseList/ReverseList/main.cpp
+++ ReverseList/ReverseList/main.cpp
@@ -7,9 +7,20 @@
 //
 
 #include <iostream>
-
+#include <List>
+using namespace std;
+//struct ListNode{
+//    int value;
+//    ListNode* next;
+//};
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    list<int> a;
+    for (int i=0; i<10; ++i) {
+        a.push_back(i);
+    }
+    a.reverse();
+    for (auto j=a.begin() ; j!=a.end();++j) {
+        cout<<*j<<endl;
+    }
     return 0;
 }
