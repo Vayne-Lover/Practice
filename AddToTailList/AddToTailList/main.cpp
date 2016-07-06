3dbd135ad5f43f5a9f6d1a8b1873f19b7b916113
Vayne-Lover <406378362@qq.com>
2016-07-06 15:44:00 +0800

    Add to tail

--- AddToTailList/AddToTailList/main.cpp
+++ AddToTailList/AddToTailList/main.cpp
@@ -1,26 +1,19 @@
 //
 //  main.cpp
-//  ReverseList
+//  AddToTailList
 //
 //  Created by 姜况童 on 7/6/16.
 //  Copyright © 2016 Vayne-Lover. All rights reserved.
 //
 
 #include <iostream>
-#include <List>
 using namespace std;
 struct ListNode{
     int value;
     ListNode* next;
 };
 int main(int argc, const char * argv[]) {
-//    list<int> a;
-//    for (int i=0; i<10; ++i) {
-//        a.push_back(i);
-//    }
-//    a.reverse();
-//    for (auto j=a.begin() ; j!=a.end();++j) {
-//        cout<<*j<<endl;
-//    }
+    // insert code here...
+    std::cout << "Hello, World!\n";
     return 0;
 }
