c72caff803f70a7f700414d9ffd6cf533e4484d1
Vayne-Lover <406378362@qq.com>
2016-07-13 13:58:30 +0800

    Binary Traversal

--- BinaryTree/BinaryTree/main.cpp
+++ BinaryTree/BinaryTree/main.cpp
@@ -7,9 +7,53 @@
 //
 
 #include <iostream>
-
+using namespace std;
+struct BinaryTree
+{
+    int value;
+    BinaryTree* left;
+    BinaryTree* right;
+};
+void AddToTree(BinaryTree** tree,int value)
+{
+    if(tree==nullptr)
+    {
+        return ;
+    }
+    *tree=new BinaryTree();
+    (*tree)->value=value;
+}
+void Traversal(BinaryTree* tree)
+{
+    if(tree==nullptr)
+    {
+        return;
+    }
+    if(tree!=nullptr)
+    {
+        cout<<tree->value<<endl;
+        Traversal(tree->left);
+        Traversal(tree->right);
+    }
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    BinaryTree* a;
+    AddToTree(&a, 1);
+    BinaryTree*b;
+    AddToTree(&b, 2);
+    BinaryTree*c;
+    AddToTree(&c, 3);
+    BinaryTree*d;
+    AddToTree(&d, 4);
+    BinaryTree*e;
+    AddToTree(&e, 5);
+    BinaryTree*f;
+    AddToTree(&f, 6);
+    a->left=b;
+    b->left=c;
+    a->right=d;
+    d->left=e;
+    d->right=f;
+    Traversal(a);
     return 0;
 }
