d66f79e5e8cbb1b4a9fd43c82e0636837ee7966e
Vayne-Lover <406378362@qq.com>
2016-07-21 09:57:04 +0800

    Add DepthOfBinaryTree()

--- DepthOfBinaryTree/DepthOfBinaryTree/main.cpp
+++ DepthOfBinaryTree/DepthOfBinaryTree/main.cpp
@@ -22,7 +22,33 @@ void AddToTree(BinaryTree** tree,int value)
     *tree=new BinaryTree();
     (*tree)->value=value;
 }
+int DepthOfTree(BinaryTree* tree)
+{
+    if(tree==nullptr)
+    {
+        return 0;
+    }
+    int left=DepthOfTree(tree->left);
+    int right=DepthOfTree(tree->right);
+    return (left>right)?(left+1):(right+1);
+}
 int main(int argc, const char * argv[]) {
-
+    BinaryTree* a;
+    AddToTree(&a, 1);
+    BinaryTree*b;
+    AddToTree(&b, 2);
+    BinaryTree*c;
+    AddToTree(&c, 3);
+    BinaryTree*d;
+    AddToTree(&d, 4);
+    BinaryTree*e;
+    AddToTree(&e, 5);
+    BinaryTree*f;
+    AddToTree(&f, 6);
+    a->left=b;
+    b->left=c;
+    a->right=d;
+    d->left=e;
+    d->right=f;
     return 0;
 }
