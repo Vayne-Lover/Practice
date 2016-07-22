b631be97af132fff1d61d68990d22e1f4f062f8f
Vayne-Lover <406378362@qq.com>
2016-07-22 18:33:45 +0800

    Finish

--- BalancedTree/BalancedTree/main.cpp
+++ BalancedTree/BalancedTree/main.cpp
@@ -7,9 +7,63 @@
 //
 
 #include <iostream>
+using namespace std;
+struct BinaryTree
+{
+    int value;
+    BinaryTree* left;
+    BinaryTree* right;
+};
+void AddToTree(BinaryTree** tree,int value)
+{
+    if(tree==nullptr)
+    {
+        return ;
+    }
+    *tree=new BinaryTree();
+    (*tree)->value=value;
+}
+int DepthOfTree(BinaryTree* tree)
+{
+    if(tree==nullptr)
+    {
+        return 0;
+    }
+    int left=DepthOfTree(tree->left);
+    int right=DepthOfTree(tree->right);
+    return (left>right)?(left+1):(right+1);
+}
+void Traversal(BinaryTree* tree)
+{
+    if(tree==nullptr)
+    {
+        return;
+    }
+    if(tree!=nullptr)
+    {
+        cout<<tree->value<<endl;
+        Traversal(tree->left);
+        Traversal(tree->right);
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    BinaryTree* a;
+    AddToTree(&a, 1);
+    BinaryTree*b;
+    AddToTree(&b, 2);
+    BinaryTree*c;
+    AddToTree(&c, 3);
+    BinaryTree*d;
+    AddToTree(&d, 4);
+    BinaryTree*e;
+    AddToTree(&e, 5);
+    BinaryTree*f;
+    AddToTree(&f, 6);
+    a->left=b;
+    b->left=c;
+    a->right=d;
+    d->left=e;
+    e->right=f;
     return 0;
 }
