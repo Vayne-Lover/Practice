f76d89e8ced91a1bc8f07b69679e5080a71c5a96
Vayne-Lover <406378362@qq.com>
2016-07-25 10:26:10 +0800

    Add XOR()

--- XOR/XOR/main.cpp
+++ XOR/XOR/main.cpp
@@ -8,11 +8,18 @@
 
 #include <iostream>
 #include <algorithm>
-void XOR()
+using namespace std;
+int XOR(int* a)
 {
-
+    int result=0;
+    for(int i=0;i<11;++i)
+    {
+        result^=a[i];
+    }
+    return result;
 }
 int main(int argc, const char * argv[]) {
-    int a[]={3,5,5,6,3,8,9,22,22,4,7,9,4,6};
+    int a[]={3,5,5,3,9,22,22,4,9,4,7};
+    cout<<XOR(a)<<endl;
     return 0;
 }
