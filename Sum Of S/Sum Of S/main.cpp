d8a23772a6f1e68cce3ad2b164e82f1a74128ab2
Vayne-Lover <406378362@qq.com>
2016-07-26 09:35:41 +0800

    Sum of s

---
+++ Sum Of S/Sum Of S/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sum Of S
+//
+//  Created by 姜况童 on 7/26/16.
+//  Copyright © 2016 Vayne-Lover. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
