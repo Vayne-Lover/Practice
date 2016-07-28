9313a0437639ddf43e32c693851ef27a908a0799
Vayne-Lover <406378362@qq.com>
2016-07-28 10:13:16 +0800

    Sum of s

--- Sum Of S/Sum Of S/main.cpp
+++ Sum Of S/Sum Of S/main.cpp
@@ -7,9 +7,34 @@
 //
 
 #include <iostream>
-
+using namespace std;
+void Sum(int* a,int s)
+{
+    if (a==nullptr)
+    {
+        return;
+    }
+    int start=0;
+    int last=(sizeof(a)/sizeof(int))-1;
+    while (start<last)
+    {
+        int sum=*(a+start)+*(a+last);
+        if(sum==s)
+        {
+            cout<<*(a+start)<<" "<<*(a+last)<<endl;
+        }
+        else if(s>sum)
+        {
+            last--;
+        }
+        else
+        {
+            start++;
+        }
+    }
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[]={1,2,3,7,11,15};
+    Sum(a, 12);
     return 0;
 }
