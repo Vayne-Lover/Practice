e1244f95bf7d997a731292d145a209c0c4e805be
Vayne-Lover <406378362@qq.com>
2016-08-04 13:45:19 +0800

    Finish function

--- StringPermutation/StringPermutation/main.cpp
+++ StringPermutation/StringPermutation/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
-
+using namespace std;
+void Permutation(char*a,char*b)
+{
+    if(*b=='\0')
+    {
+        cout<<a<<endl;
+    }
+    else
+    {
+        for(auto it=b;*it!='\0';++it)
+        {
+            char t=*it;
+            *it=*b;
+            *b=t;
+            Permutation(a, b+1);
+            t=*it;
+            *it=*b;
+            *b=t;
+        }
+    }
+}
+void StringPermutation(char* a)
+{
+    if(a==nullptr)
+    {
+        return ;
+    }
+    Permutation(a,a);
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
