c586845989ffd793347de8b1f1378a2ab4c676cd
Vayne-Lover <406378362@qq.com>
2016-08-05 12:54:09 +0800

    Add the function IsUgly().

--- UglyNumber/UglyNumber/main.cpp
+++ UglyNumber/UglyNumber/main.cpp
@@ -8,8 +8,25 @@
 
 #include <iostream>
 using namespace std;
+bool IsUgly(int n)
+{
+    while (n%2==0)
+    {
+        n/=2;
+    }
+    while (n%3==0)
+    {
+        n/=3;
+    }
+    while (n%5==0)
+    {
+        n/=5;
+    }
+    return n==1?true:false;
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout<<IsUgly(15)<<endl;
+    cout<<IsUgly(6)<<endl;
+    cout<<IsUgly(11)<<endl;
     return 0;
 }
