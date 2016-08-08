746e5ee40c74233849774f4dbc8331a8688adbad
Vayne-Lover <406378362@qq.com>
2016-08-08 12:29:19 +0800

    Add Min()

--- UglyNumber/UglyNumber/main.cpp
+++ UglyNumber/UglyNumber/main.cpp
@@ -10,6 +10,10 @@
 using namespace std;
 bool IsUgly(int n)
 {
+    if(n<=0)
+    {
+        return false;
+    }
     while (n%2==0)
     {
         n/=2;
@@ -24,12 +28,18 @@ bool IsUgly(int n)
     }
     return n==1?true:false;
 }
+int Min(int a,int b,int c)
+{
+    int temp=(a<b)?a:b;
+    int min=(temp<c)?temp:c;
+    return min;
+}
 int main(int argc, const char * argv[]) {
 //    cout<<IsUgly(15)<<endl;
 //    cout<<IsUgly(6)<<endl;
 //    cout<<IsUgly(11)<<endl;
     int count=0;
-    int i=1;
+    int i=0;
     while(count<10)
     {
         if(IsUgly(i))
