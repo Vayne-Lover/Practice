2560c7a87dc53a5ec4713243450074d68e954032
Vayne-Lover <406378362@qq.com>
2016-08-13 10:29:04 +0800

    Init.

---
+++ SequenceOfBST/SequenceOfBST/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SequenceOfBST
+//
+//  Created by 姜况童 on 8/13/16.
+//  Copyright © 2016 Vayne-Lover. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
