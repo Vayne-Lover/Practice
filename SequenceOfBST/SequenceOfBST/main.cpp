72815a14236aa5c3ed51145bbb04b2a68375c5c5
Vayne-Lover <406378362@qq.com>
2016-08-13 10:43:37 +0800

    Finish Binary Tree.

--- SequenceOfBST/SequenceOfBST/main.cpp
+++ SequenceOfBST/SequenceOfBST/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
-
+using namespace std;
+struct BinaryTree
+{
+    int value;
+    BinaryTree* left;
+    BinaryTree* right;
+};
+void AddToTree(BinaryTree** tree,int value)
+{
+    if(tree==nullptr)
+    {
+        return ;
+    }
+    *tree=new BinaryTree();
+    (*tree)->value=value;
+}
+void Traversal(BinaryTree* tree)
+{
+    if(tree==nullptr)
+    {
+        return;
+    }
+    if(tree!=nullptr)
+    {
+        cout<<tree->value<<endl;
+        Traversal(tree->left);
+        Traversal(tree->right);
+    }
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
