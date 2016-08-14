88fa0129b7c3b852d1ff1bd6267a58291c522af3
Vayne-Lover <406378362@qq.com>
2016-08-14 10:25:11 +0800

    Add ComplexListNode.

---
+++ ComplexListNodeClone/ComplexListNodeClone/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  ComplexListNodeClone
+//
+//  Created by 姜况童 on 8/14/16.
+//  Copyright © 2016 Vayne-Lover. All rights reserved.
+//
+
+#include <iostream>
+struct ComplexListNode
+{
+    int value;
+    ComplexListNode* next;
+    ComplexListNode* Sibling;
+};
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
