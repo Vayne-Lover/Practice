e96773d52e33926e84897af848f30680c4a85c8a
Vayne-Lover <406378362@qq.com>
2016-08-19 09:48:20 +0800

    Add basic structure.

--- ConvertBinarySearchTree/ConvertBinarySearchTree/main.cpp
+++ ConvertBinarySearchTree/ConvertBinarySearchTree/main.cpp
@@ -7,7 +7,35 @@
 //
 
 #include <iostream>
-
+using namespace std;
+struct BinaryTree
+{
+    int value;
+    BinaryTree* left;
+    BinaryTree* right;
+};
+void AddToTree(BinaryTree** tree,int value)
+{
+    if(tree==nullptr)
+    {
+        return ;
+    }
+    *tree=new BinaryTree();
+    (*tree)->value=value;
+}
+void Traversal(BinaryTree* tree)//Recursion
+{
+    if(tree==nullptr)
+    {
+        return;
+    }
+    if(tree!=nullptr)
+    {
+        cout<<tree->value<<endl;
+        Traversal(tree->left);
+        Traversal(tree->right);
+    }
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
