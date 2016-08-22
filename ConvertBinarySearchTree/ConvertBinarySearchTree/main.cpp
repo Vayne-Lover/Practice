dc5806f243a9977f99cef9ac5b990a240dd15716
Vayne-Lover <406378362@qq.com>
2016-08-22 10:42:13 +0800

    Finish code,fix typo,add test.

--- ConvertBinarySearchTree/ConvertBinarySearchTree/main.cpp
+++ ConvertBinarySearchTree/ConvertBinarySearchTree/main.cpp
@@ -31,8 +31,8 @@ void Traversal(BinaryTree* tree)//Recursion
     }
     if(tree!=nullptr)
     {
-        cout<<tree->value<<endl;
         Traversal(tree->left);
+        cout<<tree->value<<endl;
         Traversal(tree->right);
     }
 }
@@ -62,17 +62,44 @@ BinaryTree* Convert(BinaryTree* root)
 {
     BinaryTree* lastNode=nullptr;
     ConvertNode(root, &lastNode);
-    BinaryTree*head=nullptr;
+
     BinaryTree* headNode=lastNode;
     while(headNode!=nullptr&&headNode->left!=nullptr)
     {
-        headNode=head->left;
+        headNode=headNode->left;
     }
-    return head;
+    return headNode;
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    BinaryTree* a;
+    AddToTree(&a, 1);
+    BinaryTree*b;
+    AddToTree(&b, 2);
+    BinaryTree*c;
+    AddToTree(&c, 3);
+    BinaryTree*d;
+    AddToTree(&d, 4);
+    BinaryTree*e;
+    AddToTree(&e, 5);
+    BinaryTree*f;
+    AddToTree(&f, 6);
+    BinaryTree*g;
+    AddToTree(&g, 7);
+    d->left=b;
+    b->left=a;
+    b->right=c;
+    d->right=f;
+    f->left=e;
+    f->right=g;
+
+    //Traversal(d);
+    BinaryTree* result=Convert(d);
+    BinaryTree* temp=result;
+    while(temp)
+    {
+        cout<<temp->value<<endl;
+        temp=temp->right;
+    }
     return 0;
 }
