3e9e633a654b5ae938a264bf99809af50efe0d6a
Vayne-Lover <406378362@qq.com>
2016-08-24 11:40:53 +0800

    Finish Longestaba().

--- Longestaba/Longestaba/main.cpp
+++ Longestaba/Longestaba/main.cpp
@@ -7,9 +7,46 @@
 //
 
 #include <iostream>
-
+int Longestaba(char * s,int n)
+{
+    int i,j,max,c;
+    if(s==nullptr||n<=0)
+    {
+        return 0;
+    }
+    max=0;
+    c=0;
+    for(i=0;i<n;++i)
+    {
+        for(j=0;(i-j)>=0&&(i+j<n);++j)
+        {
+            if(s[i-j]!=s[i+j])
+            {
+                break;
+            }
+            c=j*2+1;
+        }
+        if(c>max)
+        {
+            max=c;
+        }
+        
+        for(j=0;(i-j)>=0&&(i+j+1<n);++j)
+        {
+            if(s[i-j]!=s[i+j+1])
+            {
+                break;
+            }
+            c=j*2+2;
+        }
+        if(c>max)
+        {
+            max=c;
+        }
+    }
+    return max;
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
