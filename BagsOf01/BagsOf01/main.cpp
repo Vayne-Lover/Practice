692238ee027bfc9411ec0df9ee6858309d0929b7
Vayne-Lover <406378362@qq.com>
2016-08-30 15:37:49 +0800

    Add findM.

--- BagsOf01/BagsOf01/main.cpp
+++ BagsOf01/BagsOf01/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
-
+using namespace std;
+int findM(int N,int K,int G[],int W[])
+{    int *M=new int[N+1],i,j,k;
+    for(i=0;i<N+2;i++)
+        M[i]=0;
+    for(i=0;i<K;i++)
+    {
+        for(j=N;j>=G[i];j--)
+        {
+            for(k=1;j-k*G[i]>=0;k++)
+            {
+                M[j]=M[j]>k*W[i]+M[j-k*G[i]]?M[j]:k*W[i]+M[j-k*G[i]];
+            }
+        }
+    }
+    return M[N];
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
     return 0;
 }
