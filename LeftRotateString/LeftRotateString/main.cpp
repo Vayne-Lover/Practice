e7b664f98bc4fca2a67acd356fcdf496dc398b77
Vayne-Lover <406378362@qq.com>
2016-09-07 11:24:07 +0800

    Add LeftRotateString().

--- LeftRotateString/LeftRotateString/main.cpp
+++ LeftRotateString/LeftRotateString/main.cpp
@@ -18,13 +18,29 @@ void Reverse(char* begin,char* end)
     {
         char temp=*begin;
         *begin=*end;
-        *end=*begin;
+        *end=temp;
         
         begin++,end--;
     }
 }
+char* LeftRotateString(char* str,int n,int len)
+{
+    if(str!=nullptr)
+    {
+        if(len>n&&n>0)
+        {
+            char* start1=str;
+            char* end1=str+n-1;
+            char* start2=str+n;
+            char* end2=str+len-1;
+            Reverse(start1, end1);
+            Reverse(start2, end2);
+            Reverse(start1, end2);
+        }
+    }
+    return str;
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
